97b4bef9d19c1879f48903c9ba92b7df94b030f2
AhmedMahmoud962 <am6944430@gmail.com>
2024-03-27 17:31:11 +0200

    deploy

--- src/Modal.js
+++ src/Modal.js
@@ -1,12 +1,12 @@
-// import "./LoanForm.css";
-// function Modal() {
-//   return (
-//     <div id="modal">
-//       <div id="modal-contant">
-//         <h1>The Form Has Been Submitted Successfully</h1>
-//       </div>
-//     </div>
-//   );
-// }
+import "./LoanForm.css";
+function Modal() {
+  return (
+    <div id="modal">
+      <div id="modal-contant">
+        <h1>The Form Has Been Submitted Successfully</h1>
+      </div>
+    </div>
+  );
+}
 
-// export default Modal;
+export default Modal;
